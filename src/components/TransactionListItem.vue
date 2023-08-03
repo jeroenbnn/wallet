1bdc79f5ee4fc71e74ad0cde75cd921dca413a06
Sören <hello@soerenschwert.de>
2023-08-03 15:02:16 +0200

    Display not-executed transactions as strike-through

--- src/components/TransactionListItem.vue
+++ src/components/TransactionListItem.vue
@@ -1,7 +1,7 @@
 <template>
     <button
         class="reset transaction"
-        :class="state"
+        :class="[state, {'not-executed': transaction.executionResult === false}]"
         @click="$router.push({name: 'transaction', params: {hash: transaction.transactionHash}})"
         :key="`tx-${transaction.transactionHash}`"
     >
@@ -397,6 +397,11 @@ svg {
         .amounts {
             text-decoration: line-through;
         }
+    }
+
+    &.not-executed {
+        text-decoration: line-through;
+        opacity: 0.4;
     }
 }
 
