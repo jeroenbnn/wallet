40f7a6c3ce9d9a3fe7f6dc8520eff8b5608c1410
Maxi <maximogarciamtnez@gmail.com>
2024-06-12 09:52:05 +0300

    chore: added basic store for sinpe

--- src/config/config.local.ts
+++ src/config/config.local.ts
@@ -1,3 +1,4 @@
+import { SwapAsset } from '@nimiq/libswap';
 import { ENV_DEV } from '../lib/Constants';
 
 // Note: you'll usually want to use the reactive variant exported by useConfig instead of the plain Config, especially
@@ -115,8 +116,10 @@ export default {
     },
 
     sinpeMovil: {
-        enabledSell: true,
-        enabledBuy: true,
+        enabled: true,
+        pairs: [[SwapAsset.NIM, SwapAsset.CRC]], // For now we only support selling NIM to CRC
+        sendSmsGetEndpoint: 'https://sinpemovil.sandbox.nimiqoasis.com/sms/send/{phone}',
+        verifySmsPostEndpoint: 'https://sinpemovil.sandbox.nimiqoasis.com/sms/verify',
     },
 
     goCrypto: {
