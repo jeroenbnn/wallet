e2ccd24440eceec39525aa343eb2808a405be549
Maxi <maximogarciamtnez@gmail.com>
2024-06-12 09:59:30 +0300

    chore: added support for CRC and LN_BTC in the current logic
    This commit does not add support per sé, but rather the helper functions to build the swap

--- src/components/layouts/Sidebar.vue
+++ src/components/layouts/Sidebar.vue
@@ -168,6 +168,7 @@
 import { defineComponent, ref, computed, onMounted } from '@vue/composition-api';
 import { GearIcon, Tooltip, InfoCircleIcon } from '@nimiq/vue-components';
 import { RouteName } from '@/router';
+import { assetToCurrency } from '@/lib/swap/utils/Assets';
 import AnnouncementBox from '../AnnouncementBox.vue';
 import AccountMenu from '../AccountMenu.vue';
 import PriceChart, { TimeRange } from '../PriceChart.vue';
@@ -289,7 +290,7 @@ export default defineComponent({
             return activeCurrency.value !== CryptoCurrency.NIM;
         });
         const canSellCryptoWithSinpeMovil = computed(() => { // eslint-disable-line arrow-body-style
-            if (!config.sinpeMovil.enabledSell) return false;
+            if (config.sinpeMovil.pairs.some(([from]) => assetToCurrency(from) === activeCurrency.value)) return true;
             if (!countryCode.value) return false;
             // @ts-expect-error It is fine
             return true || SINPE_MOVIL_COUNTRY_CODES.includes(countryCode.value);
